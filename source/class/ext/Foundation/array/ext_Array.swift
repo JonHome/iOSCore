88b0c5d5b0d33253b26d598ad870eac7aa1d2aaf
Jon <327493463@qq.com>
2020-05-06 15:33:55 +0800

    no message

---
+++ source/class/ext/Foundation/array/ext_Array.swift
@@ -0,0 +1,49 @@
+//
+//  ext_Array.swift
+//  core
+//
+//  Created by Jon Home on 2020/4/30.
+//  Copyright © 2020 JonHome. All rights reserved.
+//
+
+import Foundation
+
+
+extension Array {
+    
+    func get(index: Int) -> Element? {
+        if 0 <= index && index < count {
+            return self[index]
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Array where Element: Equatable {
+    
+    // Remove first collection element that is equal to the given `object`:
+    mutating func syncRemove(_ element: Element) {
+        objc_sync_enter(self)
+        if let index = firstIndex(of: element) {
+            remove(at: index)
+        }
+        objc_sync_exit(self)
+    }
+}
+
+
+extension Sequence {
+    func group<GroupingType: Hashable>(by key: (Iterator.Element) -> GroupingType) -> [[Iterator.Element]] {
+        var groups: [GroupingType: [Iterator.Element]] = [:]
+        var groupsOrder: [GroupingType] = []
+        forEach { element in
+            let key = key(element)
+            if case nil = groups[key]?.append(element) {
+                groups[key] = [element]
+                groupsOrder.append(key)
+            }
+        }
+        return groupsOrder.map { groups[$0]! }
+    }
+}
